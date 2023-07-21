e384703d8e701a90233d78401ad24718782c31bc
elanonc <nunes.elano14@gmail.com>
2023-07-20 22:26:03 -0300

    MODEL: criando model comment

--- app/Models/Comment.ts
+++ app/Models/Comment.ts
@@ -1,22 +1,15 @@
 import { DateTime } from 'luxon'
 import { BaseModel, HasMany, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
-import Comment from './Comment'
 
-export default class Moment extends BaseModel {
-  @hasMany(() => Comment)
-  public comments: HasMany<typeof Comment>
-
+export default class Comment extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
   @column()
-  public title: string
+  public username: string
 
   @column()
-  public description: string
-
-  @column()
-  public image: string
+  public text: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
